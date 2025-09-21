eaf8267d38065dd5b7c02101ecad81be1b543e63
Maciej Adamczewski <macadamczewski@gmail.com>
2025-09-21 17:01:04 +0200

    feat: start at resouces

---
+++ Raytracer/Include/Resource.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+class Resource
+{
+public:
+    [[nodiscard]] Microsoft::WRL::ComPtr<ID3D12Device5> GetDevice() const { return m_device; }
+    [[nodiscard]] Microsoft::WRL::ComPtr<ID3D12Resource> GetUnderlyingResource() const { return m_resource; }
+
+    [[nodiscard]] D3D12_RESOURCE_DESC GetDesc() const
+    { 
+        if (!m_resource)
+        {
+            return m_resource->GetDesc();
+        }
+        return D3D12_RESOURCE_DESC{};
+    }
+    
+protected:
+
+    Resource(const Microsoft::WRL::ComPtr<ID3D12Device5>& device, const D3D12_RESOURCE_DESC& desc, const D3D12_CLEAR_VALUE* clearValue = nullptr);
+    Resource(const Microsoft::WRL::ComPtr<ID3D12Device5>& device, const Microsoft::WRL::ComPtr<ID3D12Resource> &resource, const D3D12_CLEAR_VALUE* clearValue = nullptr);
+
+    virtual ~Resource() = default;
+    
+    Microsoft::WRL::ComPtr<ID3D12Device5> m_device;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_resource;
+    D3D12_FEATURE_DATA_FORMAT_SUPPORT m_formatSupport;
+    std::unique_ptr<D3D12_CLEAR_VALUE> m_clearValue;
+    std::wstring m_resourceName;
+
+private:
+    void QueryFormatSupport();
+};
---
+++ Raytracer/Source/Resource.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "Resource.h"
+
+#include "Helpers.h"
+
+Resource::Resource(const Microsoft::WRL::ComPtr<ID3D12Device5>& device, const D3D12_RESOURCE_DESC& desc, const D3D12_CLEAR_VALUE* clearValue)
+    : m_device(device)
+{
+    if (clearValue)
+    {
+        m_clearValue = std::make_unique<D3D12_CLEAR_VALUE>(*clearValue);
+    }
+
+    ThrowIfFailed(m_device->CreateCommittedResource(
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+        D3D12_HEAP_FLAG_NONE,
+        &desc,
+        D3D12_RESOURCE_STATE_COMMON,
+        m_clearValue.get(),
+        IID_PPV_ARGS(&m_resource)));
+
+    QueryFormatSupport();
+}
